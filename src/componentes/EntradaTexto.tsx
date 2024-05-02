91e4ecdde21e3ba2a93e8d59c3e33b3632ba7635
DevCantarin <123283283+DevCantarin@users.noreply.github.com>
2024-05-02 14:59:57 -0300

    Update EntradaTexto.tsx
    ASas

--- src/componentes/EntradaTexto.tsx
+++ src/componentes/EntradaTexto.tsx
@@ -1,33 +1,44 @@
 import { Input, FormControl } from "native-base";
+import { NativeSyntheticEvent, TextInputFocusEventData, ViewStyle } from "react-native";
 
 interface InputProps {
   label?: string;
   placeholder: string;
   secureTextEntry?: boolean;
   value?: string;
+  bgColor?: string;
+  inputStyle?: ViewStyle;
+  estiloTexto?: ViewStyle;
   onChangeText?: (text: string) => void;
+  onBlur?: (event: NativeSyntheticEvent<TextInputFocusEventData>) => void; // Alteração aqui
 }
 
-export function EntradaTexto ({ 
+export function EntradaTexto({ 
   label, 
   placeholder, 
   secureTextEntry = false,
   value,
-  onChangeText
-} : InputProps) : JSX.Element {
+  bgColor,
+  inputStyle, 
+  estiloTexto,
+  onChangeText,
+  onBlur
+}: InputProps): JSX.Element {
   return (
     <FormControl mt={3}>
-      {label && <FormControl.Label>{label}</FormControl.Label>}
+      {label && <FormControl.Label style={estiloTexto ? estiloTexto : null}>{label}</FormControl.Label>}
       <Input
         placeholder={placeholder}
         size="lg"
         w="100%"
         borderRadius="lg"
-        bgColor="gray.100"
+        bgColor={bgColor ? bgColor : "gray.100"}
         secureTextEntry={secureTextEntry}
         shadow={3}
-        value={value}
+        value={value}      
+        onBlur={onBlur}
         onChangeText={onChangeText}
+        style={[inputStyle, { textAlign: "center" }]}
       />
     </FormControl>
   );
