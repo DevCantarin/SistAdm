d468c12199d0482807268b140a851a7f058837f5
DevCantarin <123283283+DevCantarin@users.noreply.github.com>
2024-05-02 14:59:31 -0300

    Update Rotas.tsx
    asd

--- src/Rotas.tsx
+++ src/Rotas.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
+
 const Tab = createNativeStackNavigator();
 
 import Cadastro from "./Cadastro";
 import Login from "./Login";
 import Tabs from "./Tabs";
 import Agendamento from "./Agendamento";
+import PVS from "./Tabs/PVS"
+import PVSCadastroResidencial from "./Tabs/PVVCadastramentoResidencial";
+import PVSCadstroComercial from "./Tabs/PVVCadastramentoComercial"
+import PVSCadastroEscolar from "./Tabs/PVVCadastramentoEscolar";
 
 export default function Rotas(){
   return(
@@ -24,6 +29,18 @@ export default function Rotas(){
         />
         <Tab.Screen 
           name="Agendamento" component={Agendamento as React.FC} options={{ headerShown: false }}
+        />
+        <Tab.Screen 
+          name="PVS" component={PVS as React.FC} options={{ headerShown: false }}
+        />
+        <Tab.Screen 
+          name="Residencial" component={PVSCadastroResidencial as React.FC} options={{ headerShown: false }}
+        />
+        <Tab.Screen 
+          name="Comercial" component={PVSCadstroComercial as React.FC} options={{ headerShown: false }}
+        />
+        <Tab.Screen 
+          name="Escolar" component={PVSCadastroEscolar as React.FC} options={{ headerShown: false }}
         />
       </Tab.Navigator>
     </NavigationContainer>
