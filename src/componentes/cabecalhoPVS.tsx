ba1dd37168fead0c627da78510476c037a9bd1ab
DevCantarin <123283283+DevCantarin@users.noreply.github.com>
2024-05-02 15:00:01 -0300

    Create cabecalhoPVS.tsx
    ASasS

---
+++ src/componentes/cabecalhoPVS.tsx
@@ -0,0 +1,19 @@
+import { Image} from "native-base";
+import { StyleSheet } from 'react-native';
+const estilos = StyleSheet.create({
+  cabecalho:{
+    width: "100%",
+    height: 200,
+    borderColor: "black", // Cor da borda
+    borderWidth: 2
+  }
+}
+)
+
+export function CabecalhoPVS() {
+  return (
+    <>
+      <Image style={estilos.cabecalho} alt="logo da Vizinhança Solidária" source={require("../../assets/logopmesp.png")} />
+    </>
+  );
+}
